6a88543a779041207a02ecb4d6b3cc3878035cbc
shekodn <sdiazn19@gmail.com>
2016-11-21 15:46:57 -0600

    fixes migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523233003) do
+ActiveRecord::Schema.define(version: 20161121213157) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
